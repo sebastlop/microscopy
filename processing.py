fbfce5b2006982a56f34c32961195f68ad0bad53
sebastlop <sebastlop@gmail.com>
2024-06-25 18:55:22 -0300

    something

--- processing.py
+++ processing.py
@@ -1,7 +1,7 @@
 import numpy as np
 from read_images import read_folder
 import matplotlib.pyplot as plt
-from scipy.signal import convolve
+from scipy.signal import convolve, find_peaks
 
 DELTA_TIME = 0.1
 
@@ -28,25 +28,44 @@ def gaussian_denoising(img, kernel_size=41):
     convolved = np.maximum(convolved,0)
     return convolved
 
-def gaussian_border_detection(img, kernel_size=41):
+def gaussian_border_detection(img, kernel_size=3, sign = 1):
     x = np.linspace(-1,1,kernel_size)
-    kernel = x*np.exp(-(x**2)/0.25**2)/(2*np.pi*0.25)
+    kernel = sign*x*np.exp(-(x**2)/0.25**2)/(2*np.pi*0.25)
     convolved = convolve(img, kernel, mode='same')
     return convolved
 
+def get_mesh_coordinates(max_x, max_y, dx, dy):
+    x = np.arange(np.int32(dx), max_x-np.int32(dx), np.int32(dx))
+    y = np.arange(np.int32(dy), max_y-np.int32(dy), np.int32(dy))
+    X,Y = np.meshgrid(x,y)
+    return X, Y
+
 if __name__ == '__main__':
     raw_imgs = read_folder('./img_fase', kind='fase')
-    for idx in range(100,130):
-        fig, axs = plt.subplots(1,2)
-        axs[0].imshow(raw_imgs[idx])
-        axs[1].imshow(raw_imgs[idx+1]-raw_imgs[idx])
-        plt.show()
-    exit()
+    NX = raw_imgs[1].shape[0]
+    NY = raw_imgs[1].shape[1]
+    derivs = {}
+    for k in range(2,len(list(raw_imgs.keys()))):
+        derivs[k] = raw_imgs[k]-raw_imgs[k-1]
+        mask = derivs[k] > 150
+        derivs[k][mask] = 0
 
 
+    fig, axs = plt.subplots(1,2)
+    frame = 100
+    axs[0].set_title(f'frame{frame}')
+    axs[0].imshow(derivs[frame].T)
+    print(derivs[frame].shape)
 
-    plt.plot(get_pixel_in_time(raw_imgs, x_idx=13, y_idx=250), alpha = 0.5)
-    plt.plot(get_pixel_in_time(raw_imgs, x_idx=16, y_idx=250), alpha = 0.5)
-    plt.plot(get_pixel_in_time(raw_imgs, x_idx=19, y_idx=250), alpha = 0.5)
+    # sobre eje
+    xses, yses = get_mesh_coordinates(max_x= NX, max_y= NY, dx= 50, dy= 50)
+    axs[0].scatter(xses, yses, marker='x', color='r', s=5)
+    for i in range(xses.shape[1]):
+        denoised = gaussian_denoising(get_pixel_in_time(derivs, x_idx=xses[-1,i], y_idx=yses[-1,i]), kernel_size=21)
+
+        peaks, props = find_peaks(denoised, distance = 10, prominence = 0.5, width=2)
+        axs[1].plot(denoised+i, alpha = 0.5)
+        axs[1].vlines(peaks, i, props["prominences"]+i, ls='dotted')
+        axs[1].hlines(y=props["prominences"]+i, xmin=props["left_ips"],xmax=props["right_ips"])
 
     plt.show()
