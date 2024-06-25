1416e45f0517e96ee1beccd4409325923d9e0020
sebastlop <sebastlop@gmail.com>
2024-06-25 01:21:49 -0300

    nothing here

--- processing.py
+++ processing.py
@@ -1,6 +1,7 @@
 import numpy as np
 from read_images import read_folder
 import matplotlib.pyplot as plt
+from scipy.signal import convolve
 
 DELTA_TIME = 0.1
 
@@ -19,12 +20,33 @@ def correlate_signals(s1, s2):
         correlog = np.hstack([correlog,np.correlate(s1[d:],s2[:-d],mode='valid')[0]])
     return correlog
 
+def gaussian_denoising(img, kernel_size=41):
+    x = np.linspace(-1,1,kernel_size)
+    kernel = np.exp(-(x**2)/0.25**2)/(2*np.pi*0.25)
+    convolved = convolve(img, kernel, mode='same')
+    convolved= np.minimum((convolved-convolved.min())/(convolved.max()-convolved.min()),1)
+    convolved = np.maximum(convolved,0)
+    return convolved
+
+def gaussian_border_detection(img, kernel_size=41):
+    x = np.linspace(-1,1,kernel_size)
+    kernel = x*np.exp(-(x**2)/0.25**2)/(2*np.pi*0.25)
+    convolved = convolve(img, kernel, mode='same')
+    return convolved
+
 if __name__ == '__main__':
-    raw_imgs = read_folder('./img_modulo', kind='modulo')
+    raw_imgs = read_folder('./img_fase', kind='fase')
+    for idx in range(100,130):
+        fig, axs = plt.subplots(1,2)
+        axs[0].imshow(raw_imgs[idx])
+        axs[1].imshow(raw_imgs[idx+1]-raw_imgs[idx])
+        plt.show()
+    exit()
 
-    plt.plot(get_pixel_in_time(raw_imgs, x_idx=10, y_idx=10), alpha = 0.5)
-    plt.plot(get_pixel_in_time(raw_imgs, x_idx=10, y_idx=55), alpha = 0.5)
-    plt.show()
 
-    plt.plot(correlate_signals(get_pixel_in_time(raw_imgs, x_idx=10, y_idx=10), get_pixel_in_time(raw_imgs, x_idx=10, y_idx=55)))
+
+    plt.plot(get_pixel_in_time(raw_imgs, x_idx=13, y_idx=250), alpha = 0.5)
+    plt.plot(get_pixel_in_time(raw_imgs, x_idx=16, y_idx=250), alpha = 0.5)
+    plt.plot(get_pixel_in_time(raw_imgs, x_idx=19, y_idx=250), alpha = 0.5)
+
     plt.show()
